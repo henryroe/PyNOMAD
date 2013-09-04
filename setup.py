dd400fa36883dc730fd3512f56cb5e3b3089ee3d
Henry Roe <hroe@hroe.me>
2013-09-04 15:50:36 -0700

    updated for setup.py sdist usage

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-# TODO: check for a ~/.nomad and create/modify as needed
-
 setup(name='nomad',
       # TODO: check for additional meta-data to add here, following from:
       #       http://docs.python.org/2/distutils/setupscript.html#additional-meta-data
@@ -9,6 +7,6 @@ setup(name='nomad',
       description='Routines for accessing a self-hosted local copy of the USNO NOMAD catalog',
       author='Henry Roe',
       author_email='hroe@hroe.me',
-      url='TODO',
+      url='TODO',  # TODO: this should probably be a link to the eventual github page? or projectpita?
       py_modules=['nomad'],
       install_requires=['pandas>=0.10.1'])
