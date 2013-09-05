cfd68a33e39b17700a00f6627175799867b1d370
Henry Roe <hroe@hroe.me>
2013-09-04 19:20:06 -0700

    Reorganization of project for distributing via PyPI - attempt #1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='nomad',
-      # TODO: check for additional meta-data to add here, following from:
-      #       http://docs.python.org/2/distutils/setupscript.html#additional-meta-data
-      version='0.1',
+      version='0.1.0',
       description='Routines for accessing a self-hosted local copy of the USNO NOMAD catalog',
+      long_description=open('README.txt').read(),
       author='Henry Roe',
       author_email='hroe@hroe.me',
-      url='TODO',  # TODO: this should probably be a link to the eventual github page? or projectpita?
+      url='http://pypi.python.org/pypi/PyNOMAD',
+      license='LICENSE.txt',
       py_modules=['nomad'],
       install_requires=['pandas>=0.10.1'])
